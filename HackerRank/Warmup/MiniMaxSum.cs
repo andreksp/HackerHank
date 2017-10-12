46f92207c4a7011ef0e382d7cddc40532c9f82fd
Andre <andreksp@gmail.com>
2017-10-12 18:27:52 -0300

    new files

---
+++ HackerRank/Warmup/MiniMaxSum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Implementation
+{
+    class MiniMaxSum
+    {
+        static void Main(String[] args)
+        {
+            string[] arr_temp = Console.ReadLine().Split(' ');
+            int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+
+            List<long> values = arr.ToList().ConvertAll(i => (long)i);
+
+            Console.WriteLine(values.OrderBy(p => p).Take(4).Sum().ToString() + " " + values.OrderByDescending(p => p).Take(4).Sum().ToString());
+
+
+        }
+    }
+}
